87fe90c220832433234a7e6aeec4a4bcb9b11dff
lucas <lucaslpa12345@gmail.com>
2024-06-30 22:31:54 -0300

    adicionando componente touchableopacitybox

---
+++ src/components/TouchableOpacityBox.tsx
@@ -0,0 +1,30 @@
+import {
+  createRestyleComponent,
+  layout,
+  border,
+  backgroundColor,
+  spacing,
+  spacingShorthand,
+  SpacingProps,
+  BackgroundColorProps,
+  BorderProps,
+  LayoutProps,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import {Theme} from '../theme';
+
+export type TouchableOpacityBoxProps = TouchableOpacityProps &
+  LayoutProps<Theme> &
+  SpacingProps<Theme> &
+  BackgroundColorProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme>;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [layout, border, backgroundColor, spacing, spacingShorthand],
+  TouchableOpacity,
+);
